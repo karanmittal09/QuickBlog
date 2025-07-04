a473c9971dc41f7180720c7e6c9dce7962f515a8
karanmittal09 <151923351+karanmittal09@users.noreply.github.com>
2025-07-04 18:29:48 +0530

    First Commit - React setup

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Blog from './pages/Blog'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <div>
+      <Routes> 
+        <Route path='/' element={<Home/>}/>
+        <Route path='/blog' element={<Blog/>}/>
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
